260dd07f5b650907144284cab5cb11f5d364469d
LAPTOP-S53LEI9T\lenovo <mairahfadya@gmail.com>
2021-11-10 12:31:39 +0800

    pengertian vcs

---
+++ resources/views/vcs.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+	Version control adalah sebuah sistem yang merekam perubahan-perubahan dari sebuah berkas atau sekumpulan berkas dari waktu ke waktu sehingga Anda dapat menilik kembali versi khusus suatu saat nanti.
+
+	GIT
+CVS
+SVN
+Mercurial
+Monotone
+</body>
+</html>
